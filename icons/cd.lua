3ae54c43acf649b7d42fb505b8fa3b5f39d9dfff
Thorben Heemsoth <T.Heemsoth@outlook.com>
2025-10-11 17:17:17 +0200

    - Rearranged existing Icons and insert new Icons

---
+++ icons/cd.lua
@@ -0,0 +1,18 @@
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local lib_name = "LibCustomIcons"
+local lib = _G[lib_name]
+local s = lib.GetStaticTable()
+local a = lib.GetAnimatedTable()
+
+s["@CroNoS.TOG"] = "LibCustomIcons/icons/cd/cronos_tog.dds"
+s["@NamelessOne87"] = "LibCustomIcons/icons/cd/namelessone87.dds"
+s["@Muckinator"] = "LibCustomIcons/icons/cd/muckinator.dds"
+s["@Ney.rin"] = "LibCustomIcons/icons/cd/ney_rin.dds"
+s["@Sacriefy"] = "LibCustomIcons/icons/cd/sacriefy.dds"
+s["@Seolaelis"] = "LibCustomIcons/icons/cd/seolaelis.dds"
+s["@SirBoost"] = "LibCustomIcons/icons/cd/sirboost.dds"
+s["@SkarkOo"] = "LibCustomIcons/icons/cd/skarkoo.dds"
+s["@Wolf384"] = "LibCustomIcons/icons/cd/Wolf384.dds"
+s["@xxFeyLamiaxx"] = "LibCustomIcons/icons/cd/xxfeylamiaxx.dds"
