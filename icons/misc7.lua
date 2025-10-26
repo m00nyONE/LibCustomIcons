3ba271ad7240211a071bbc65a35839e6b5af5545
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-26 14:02:47 +0100

    add/edit/update users

--- icons/misc7.lua
+++ icons/misc7.lua
@@ -293,4 +293,5 @@ s["@Kebahah"] = "LibCustomIcons/icons/misc7/Kebahah.dds"
 s["@Valjaria"] = "LibCustomIcons/icons/misc7/Valjaria.dds"
 s["@mikaleidoscorpion"] = "LibCustomIcons/icons/misc7/mikaleidoscorpion.dds"
 a["@coatsncheeze"] = {"LibCustomIcons/icons/misc7/coatsncheeze_anim.dds", 19, 2, 15}
+s["@Esstarossa"] = "LibCustomIcons/icons/misc7/Esstarossa.dds"
 s["@SteveIrwin_ESO"] = "LibCustomIcons/icons/misc7/SteveIrwin.dds"
