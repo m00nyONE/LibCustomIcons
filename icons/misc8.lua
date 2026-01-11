b8bbe5edf351fb29195cf650b23ffb7d397c1adf
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2026-01-11 11:40:49 +0100

    add/edit/update users

---
+++ icons/misc8.lua
@@ -0,0 +1,9 @@
+-- SPDX-FileCopyrightText: 2026 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local lib_name = "LibCustomIcons"
+local lib = _G[lib_name]
+local s = lib.GetStaticTable()
+local a = lib.GetAnimatedTable()
+
+s["@RedBoul"] = "LibCustomIcons/icons/misc8/RedBoul.dds"
