d9800fcda966b0f86ea4e34fb4bf80c76b571c0d
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2026-01-18 11:48:51 +0100

    add/edit/update users

--- icons/misc8.lua
+++ icons/misc8.lua
@@ -9,3 +9,4 @@ local a = lib.GetAnimatedTable()
 s["@RedBoul"] = "LibCustomIcons/icons/misc8/RedBoul.dds"
 s["@Soul_Hagans"] = "LibCustomIcons/icons/misc8/Soul_Hagans.dds"
 s["@Ukmadorian"] = "LibCustomIcons/icons/misc8/Ukmadorian.dds"
+a["@Kuroko29012"] = {"LibCustomIcons/icons/misc8/Kuroko29012_anim.dds", 17, 1, 10}
