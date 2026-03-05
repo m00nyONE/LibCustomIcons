99d04065354c54651fd189a8c3d0727ebbf32334
Flatlin3d <mouricemol@gmail.com>
2026-03-05 19:26:13 +0100

    new animated icon

--- icons/misc8.lua
+++ icons/misc8.lua
@@ -50,3 +50,4 @@ a["@Nickx9"] = {"LibCustomIcons/icons/misc8/Nickx9_anim.dds", 11, 1, 4}
 a["@LeeWangcai"] = {"LibCustomIcons/icons/misc8/LeeWangcai_anim.dds", 15, 2, 8}
 a["@Saarz"] = {"LibCustomIcons/icons/misc8/Saarz_anim.dds", 16, 1, 9}
 a["@ShuguBota"] = {"LibCustomIcons/icons/misc8/ShuguBota_anim.dds", 20, 1, 25}
+a["@FIatIining"] = {"LibCustomIcons/icons/misc8/FIatIining_anim.dds", 45, 1, 30}
