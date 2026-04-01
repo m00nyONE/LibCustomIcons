2a3bb12aff45ddd5daa81769c8a74863dee78be8
Taku3mi-T93 <shoozer@nightcord.org>
2026-04-02 02:49:56 +0800

    Update misc8.lua

--- icons/misc8.lua
+++ icons/misc8.lua
@@ -76,3 +76,5 @@ s["@paradiys"] = "LibCustomIcons/icons/misc8/paradiys.dds"
 a["@Kaioshin14"] = {"LibCustomIcons/icons/misc8/Kaioshin14_anim.dds", 9, 1, 10}
 s["@CheshireNeko"] = "LibCustomIcons/icons/misc8/CheshireNeko.dds"
 a["@SLIMLAC2000"] = {"LibCustomIcons/icons/misc8/SLIMLAC2000_anim.dds", 11, 4, 33}
+s["@Cheesecattac"] = "LibCustomIcons/icons/misc8/Cheesecattac.dds"
+s["@Taku3mi"] = "LibCustomIcons/icons/misc8/Taku3mi.dds"
