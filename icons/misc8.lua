f8b05a3c75a1ed0e201b280681d39cc9163adc12
Fer-Torres19 <2903493837@qq.com>
2026-06-20 12:21:21 +0800

    Add custom icon mapping for @Fer_Torres

--- icons/misc8.lua
+++ icons/misc8.lua
@@ -177,3 +177,4 @@ a["@ObiJovi"] = {"LibCustomIcons/icons/misc8/ObiJovi_anim.dds", 5, 1, 5.5}
 a["@Kansxi"] = {"LibCustomIcons/icons/misc8/Kansxi_anim.dds", 47, 1, 14}
 s["@Notpure2"] = "LibCustomIcons/icons/misc8/Notpure2.dds"
 a["@Notpure2"] = {"LibCustomIcons/icons/misc8/Notpure2_anim.dds", 4, 7, 28}
+s["@Fer_Torres"] = "LibCustomIcons/icons/misc8/@Fer_Torres.dds"
